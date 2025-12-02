2002322d2cf1c0eb81b0f608b4a950ee2f0f0d7a
anurag <yourboyanurag777@gmail.com>
2025-12-02 17:58:19 +0530

    Added notfound

--- app/chat/[chatId]/not-found.tsx
+++ app/chat/[chatId]/not-found.tsx
@@ -7,11 +7,11 @@ export default function NotFoud() {
         404
       </div>
       <div className="text-xl font-semibold md:mt-4 mt-2">Page Not Found</div>
-      <div className="text-base text-muted-foreground text-center">
-        Oops! The page you're looking for doesn't exist or has been moved.
+      <div className="text-base text-muted-foreground text-center ">
+        Oops! The page  dosen't exist 
       </div>
       <div className="mt-1">
-        <GoBack />
+        <GoBack  className="px-4 rounded-md hover:bg-gray-200"/>
       </div>
     </div>
   );
