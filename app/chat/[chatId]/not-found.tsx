f89f396e8b799640a58e9b653e75547602d2262e
Anurag Sharma <yourboyanurag777@gmail.com>
2026-01-20 10:04:11 +0530

    Update not-found.tsx

--- app/chat/[chatId]/not-found.tsx
+++ app/chat/[chatId]/not-found.tsx
@@ -1,18 +1,22 @@
 import { GoBack } from "@/components/button/go-back";
 
-export default function NotFoud() {
+export default function NotFound() {
   return (
-    <div className="flex flex-col justify-center items-center align-middle gap-y-2 font-sans h-screen px-5">
-      <div className="flex flex-col items-center align-middle md:text-9xl text-8xl md:font-bold font-bold text-muted-foreground/60">
+    <div className="flex flex-col justify-center items-center gap-y-2 font-sans h-screen px-5">
+      <div className="flex flex-col items-center md:text-9xl text-8xl font-bold text-muted-foreground/60">
         404
       </div>
-      <div className="text-xl font-semibold md:mt-4 mt-2">Page Not Found</div>
-      <div className="text-base text-muted-foreground text-center ">
-        Oops! The page  dosen't exist  and might have been moved or deleted.
+
+      <div className="text-xl font-semibold mt-2 md:mt-4">
+        Page Not Found
       </div>
-      <div className="mt-1">
-        <GoBack  className="px-4 rounded-md hover:bg-gray-200"/>
-      
+
+      <div className="text-base text-muted-foreground text-center">
+        Oops! The page doesn&apos;t exist and might have been moved or deleted.
+      </div>
+
+      <div className="mt-2">
+        <GoBack className="px-4 rounded-md hover:bg-gray-200" />
       </div>
     </div>
   );
