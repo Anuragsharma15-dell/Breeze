1dfaccb9bb8ca18448cb39341384df5e98b31786
anurag <yourboyanurag777@gmail.com>
2026-01-21 16:37:35 +0530

    not found

--- app/chat/[chatId]/not-found.tsx
+++ app/chat/[chatId]/not-found.tsx
@@ -1,6 +1,7 @@
 import { GoBack } from "@/components/button/go-back";
 
 export default function NotFoud() {
+  
   return (
     <div className="flex flex-col justify-center items-center align-middle gap-y-2 font-sans h-screen px-5">
       <div className="flex flex-col items-center align-middle md:text-9xl text-8xl md:font-bold font-bold text-muted-foreground/60">
