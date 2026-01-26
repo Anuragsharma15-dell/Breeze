036514a9a31105ac6b69ac3e88d1639fa919a4e0
anurag <yourboyanurag777@gmail.com>
2026-01-26 11:38:42 +0530

    messages

--- app/chat/[chatId]/not-found.tsx
+++ app/chat/[chatId]/not-found.tsx
@@ -6,6 +6,7 @@ export default function NotFoud() {
     <div className="flex flex-col justify-center items-center align-middle gap-y-2 font-sans h-screen px-5">
       <div className="flex flex-col items-center align-middle md:text-9xl text-8xl md:font-bold font-bold text-muted-foreground/60">
         404
+        
       </div>
       <div className="text-xl font-semibold md:mt-4 mt-2">Page Not Found</div>
       <div className="text-base text-muted-foreground text-center ">
